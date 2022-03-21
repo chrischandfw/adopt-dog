44da29add455f458c39ee22292defd62fb2fe618
chris_chan <chrischandfw@gmail.com>
2022-03-21 14:42:23 -0500

    added wait line to async function

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ import { renderDogCard } from './render-utils.js';
 const dogListContainer = document.getElementById('dog-list-container');
 
 // on load
+window.addEventListener('load', async() => {
+    const doggies = await getDogs();
+
+	
+});
+
+
 // fetch all dogs
 // render and append all dog cards to the container
